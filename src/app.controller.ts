e8b203228d11d9c1f71f515e914d423c80efe779
sumancfc <sumanstha999@gmail.com>
2025-01-04 11:29:57 +0100

    Playing with controller, services and modules

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHelloWorld(): string {
     return this.appService.getHello();
   }
 }
